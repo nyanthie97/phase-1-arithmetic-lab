37e03fa662bd29b66995b6e5679d29f726aff11a
Nyanthie Mayang Gatluak <nyanthie.mayang@student.moringaschool.com>
2022-06-12 00:30:28 +0300

    commit tree 419aa32b4832ed2b4d204702e2428c671a29105f

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
+
+let random = Math.ceil(Math.random() * 10)
+let num3 = 28
+let num4 = 6
+let mod = num3 % num4
+let max = Math.max( 4, 8, 12, 16,20)
